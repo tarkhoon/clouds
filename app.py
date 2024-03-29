fac792439e4a1cbd5a6e593d7ff47bb71dd32b11
Student Stud <student@stud.nefu>
2024-03-29 12:43:42 +0900

    third

--- app.py
+++ app.py
@@ -7,24 +7,3 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
     return "Hello, World!"
-
-test_app.py
-import pytest
-from app import app as app
-
-
-@pytest.fixture
-def webapp():
-    app.config['TESTING'] = True
-    yield app
-
-
-@pytest.fixture()
-def client(webapp):
-    return webapp.test_client()
-
-
-def test_index_page(client):
-    res = client.get('/')
-    assert res.status_code == 200
-    assert 'Hello, World' in res.text
